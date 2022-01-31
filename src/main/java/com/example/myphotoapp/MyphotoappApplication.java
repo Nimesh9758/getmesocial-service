003e406b0d9efd78aff0b3b9ee9d5c59ec1f28f2
Nimesh9758 <82478954+Nimesh9758@users.noreply.github.com>
2022-01-31 12:40:41 -0500

    final push for assignment 2

--- src/main/java/com/example/myphotoapp/MyphotoappApplication.java
+++ src/main/java/com/example/myphotoapp/MyphotoappApplication.java
@@ -2,9 +2,10 @@ package com.example.myphotoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-
+@EnableSwagger2
 @SpringBootApplication
 
 public class MyphotoappApplication {
